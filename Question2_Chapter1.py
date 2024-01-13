94f1bc92fde406e945f495862ebdfea4c2692125
vinh28995 <54851206+vinh28995@users.noreply.github.com>
2024-01-13 16:26:42 +0930

    Add files via upload

--- Question2_Chapter1.py
+++ Question2_Chapter1.py
@@ -10,25 +10,33 @@ current_time = int(time.time())
 generated_number = (current_time % 100) + 50
 if generated_number % 2 == 0:
     generated_number += 10
-
+print(generated_number)
 # Load the image
-img = Image.open(os.getcwd()+'\chapter1.jpg')
+original_image = Image.open(os.getcwd()+'/chapter1.jpg')
+total_r = 0
+def increase_rgb(image, value):
+    # Split the image into channels
+    r, g, b = image.split()
+    print(r)
+    # Convert channels to "L" mode which has pixel values 0-255
+    r, g, b = r.point(lambda i: i + value), g.point(lambda i: i + value), b.point(lambda i: i + value)
+    # Merge the channels back
+    return Image.merge("RGB", (r, g, b))
 
-# Convert image to numpy array
-img_array = np.array(img)
 
-# Ensure we don't go over the 255 limit for pixel values after addition
-clipped_img_array = np.clip(img_array + generated_number, 0, 255)
-
-# Convert the modified array back to an image
-new_img = Image.fromarray(clipped_img_array.astype('uint8'))
-
+# Apply the increase to the RGB values using the generated number
+modified_image = increase_rgb(original_image, generated_number)
 # Save the new image with converted pixels
-output_image_path = os.getcwd()+'\chapter1out.jpg'
-new_img.save(output_image_path)
-
+output_image_path = os.getcwd()+'/chapter1.jpg'
+modified_image.save(output_image_path)
+width, height = modified_image.size
+for y in range(height):
+    for x in range(width):
+        # Get the original pixel values (r, g, b)
+        r,g,b = modified_image.getpixel((x, y))
+        total_r = total_r + r
 # Calculate the sum of the red channel values in the new image
-sum_red_values = np.sum(clipped_img_array[:, :, 0])
+#sum_red_values = np.sum(clipped_img_array[:, :, 0])
 
 # Output the sum of the red channel values
-print(sum_red_values)
+print(total_r)
