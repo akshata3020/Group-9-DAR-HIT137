1d95dc7fcb36dff3a7deb69fb32bd8776b073449
akshata3020 <150230799+akshata3020@users.noreply.github.com>
2024-01-13 10:15:13 +0930

    Group-9-DAR-HIT137-Question3-P1.py

--- Group-9-DAR-HIT137-Question3-P1.py
+++ Group-9-DAR-HIT137-Question3-P1.py
@@ -1,7 +1,7 @@
 def decrypt(encrypted_text, key):
     decrypted_text = ""
     for char in encrypted_text:
-        if char.isalpha():  # 检查字符是否为字母
+        if char.isalpha():  #Check if the character is key 
             shifted = ord(char) - key
             if char.islower():
                 if shifted < ord('a'):
@@ -18,7 +18,7 @@ def decrypt(encrypted_text, key):
             decrypted_text += char
     return decrypted_text
 
-# 使用密钥13来解密文本
+# using key 13 to decrypt the text
 with open('encrypted_text.txt', 'r') as file:
     encrypted_text = file.read()
 decrypted_text = decrypt(encrypted_text, 13)
