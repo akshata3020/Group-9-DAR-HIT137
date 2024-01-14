c071b9dd5b0d0167c2d6d86489ac810b6f814685
vinh28995 <54851206+vinh28995@users.noreply.github.com>
2024-01-14 14:30:11 +0930

    Add files via upload

--- Question2.py
+++ Question2.py
@@ -1,3 +1,58 @@
+from PIL import Image
+import numpy as np
+import time
+import os
+import sys
+print(os.getcwd())
+# Get the full path of the current file
+file_path = os.path.realpath(__file__)
+
+# Extract the directory path
+directory_path = os.path.dirname(file_path)
+
+file_path = directory_path + '/chapter1.jpg'
+
+# Question 2 Chapter 1
+
+# Calculate the current time and the generated number according to the provided algorithm
+current_time = int(time.time())
+generated_number = (current_time % 100) + 50
+if generated_number % 2 == 0:
+    generated_number += 10
+print(generated_number)
+# Load the image
+original_image = Image.open(file_path)
+total_r = 0
+def increase_rgb(image, value):
+    # Split the image into channels
+    r, g, b = image.split()
+    print(r)
+    # Convert channels to "L" mode which has pixel values 0-255
+    r, g, b = r.point(lambda i: i + value), g.point(lambda i: i + value), b.point(lambda i: i + value)
+    # Merge the channels back
+    return Image.merge("RGB", (r, g, b))
+
+
+# Apply the increase to the RGB values using the generated number
+modified_image = increase_rgb(original_image, generated_number)
+# Save the new image with converted pixels
+output_image_path = os.getcwd()+'/chapter1out.jpg'
+modified_image.save(output_image_path)
+width, height = modified_image.size
+for y in range(height):
+    for x in range(width):
+        # Get the original pixel values (r, g, b)
+        r,g,b = modified_image.getpixel((x, y))
+        total_r = total_r + r
+# Calculate the sum of the red channel values in the new image
+#sum_red_values = np.sum(clipped_img_array[:, :, 0])
+
+# Output the sum of the red channel values
+print(total_r)
+
+
+# Question 2 Chapter 2
+
 s = '56aAww1984sktr235270aYmn145ss785fsq31D0'
 
 # declare variables
